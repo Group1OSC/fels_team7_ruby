d1c81a1f8acd4c3bab840d3e262ba7ea8f8b9c24
thanhluan <vuvanlyth@gmail.com>
2015-11-21 09:37:47 +0700

    change

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,4 +6,8 @@ class WordsController < ApplicationController
   		format.json { render json: @words.to_json }
   	end
   end
+  def show 
+  	@word = Word.find params[:id]
+  	@word_answer = Word_answer.find prams[:id]
+  end
 end
